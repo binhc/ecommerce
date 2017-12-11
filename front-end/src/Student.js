89f4aaa8879595e04adb9c6401c5b60084205e5c
Binh Chung <binh.chung15@gmail.com>
2017-12-11 12:30:27 -0500

    set up router

---
+++ front-end/src/Student.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Student extends Component{
+   render(){
+       var students = [
+           'Casey', 
+           'Eddie', 
+           'Jamie', 
+           'Valerie'
+       ];
+       var studentArray = students.map((student)=>{
+           return (<li>{student}</li>)
+       })
+       return(
+        <div>{studentArray}</div>
+       )
+   }
+}
+
+export default Student;
